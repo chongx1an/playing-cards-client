1dde1c12c161e049ff14e6e099b2e25eec0582ff
Chong Xian <chongxian@easystore.co>
2021-09-28 17:32:25 +0800

    Initialise project

---
+++ src/components/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Intro() {
+    return (
+        <div>
+            <input type="number"></input>
+            <button>Start</button>
+        </div>
+    )
+}
